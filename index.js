a41cc78c7b1d28f4aea008ede2cde305ed8e047e
Sergio Berdeja <sergioberdeja@gmail.com>
2022-03-16 15:25:46 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block <= 42) {
+        return 42 - block
+    }
+    else {
+        return block - 42
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start < end) {
+        return (end - start) * 264
+    }
+    else {
+        return (start - end) * 264
+    }
+}
+
+function calculatesFarePrice(start, end) {
+    let dist = distanceTravelledInFeet(start, end)
+    if (dist > 2500) {
+        return 'cannot travel that far'
+    }
+    else if (dist <= 400) {
+        return 0
+    }
+    else if (dist >= 2000) {
+        return 25
+    }
+    else {
+        return 0.02 * (dist-400)
+    }
+}
